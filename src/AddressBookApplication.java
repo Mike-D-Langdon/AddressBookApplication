ddfcff509482efa1f9e46104fd63029477e49d21
Mike Langdon <mlangdon2@horizon.csueastbay.edu>
2021-02-24 15:19:23 -0800

    Updated the main() method to call a menu prompt and set it to loop until the 'quit' option is selected.  Fixed the prompt_zip method in Menu to better check the value being an integer.  Added methods corresponding to each menu selection.  Changed the find() method to allow a subset of the address book list to be displayed for the search term, allowing multiple entries to be found.  In AddressBook, added remove() and readFromFile() methods.  Fixed some JavaDoc comments in all classes.  Added some Junit test classes, but they aren't implemented yet.

--- src/AddressBookApplication.java
+++ src/AddressBookApplication.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 /**
@@ -8,22 +6,22 @@ import java.io.IOException;
  * @since 02/15/2021
  */
 public class AddressBookApplication {
-    // Creating a static class variable for the address book:
-    static AddressBook ab = AddressBook.getInstance();
-    /**
-     * The main method.  This calls the other methods to initialize the address book.
-     * @param args Command line arguments.  Not used.
-     */
-    public static void main(String[] args) {
 
-//        try {
-//            init("/Users/room237/Desktop/Spring2021/CS401/AddressBookApplication/src/file_with_addresses.txt");
-//            ab.list();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        initAddressBookExercise(ab);
+    /**
+     * Starting point of the app.  Displays a menu of options from which a user
+     * can select, and runs the command selected.  After the command finishes the
+     * menu is displayed again.  The program ends when the user selects the option
+     * to quit.
+     * @param args command line arguments.  Not used.
+     * @throws IOException if the file is unable to be opened.
+     */
+    public static void main(String[] args) throws IOException {
+        // Default the selection with 'e', which lists the current address entries.
+        char selection = 'e';
+        // Display the menu options until 'f' for 'quit' is selected.
+        while (selection != 'f') {
+            selection = Menu.displayMenu();
+        }
     }
 
     /**
@@ -41,174 +39,5 @@ public class AddressBookApplication {
         addressBook.add(addressEntry2);
 
         addressBook.list();
-    }
-
-    /**
-     * Initializes an address book from a given file.
-     * @param filename Name of the file containing address entries.  Given as a <code>String</code>
-     * @throws IOException If file is not found or bad input is given an exception is thrown.
-     */
-    public static void init(String filename) throws IOException {
-        // Stores a single line read from the file.
-        String line;
-        // Stores the first name, given on first (9th, 17th...) line of file, if valid.
-        String lineFirstName;
-        // Stores the last name, given on second (10th, 18th...) line of file, if valid.
-        String lineLastName;
-        // Stores the street, given on third (11th, 19th...) line of file, if valid.
-        String lineStreet;
-        // Stores the city, given on the fourth (12th, 20th...) line of file, if valid.
-        String lineCity;
-        // Stores the state, given on the fifth (13th, 21st...) line of file, if valid.
-        String lineState;
-        // Stores the zip, given on the sixth (14th, 22nd...) line of file, if valid.
-        Integer lineZip;
-        // Stores the phone number, given on the seventh (15th, 23rd...) line of file, if valid.
-        String linePhone;
-        // Stores the email, given on the eighth (16th, 24th...) line of file, if valid.
-        String lineEmail;
-
-        // Open file with FileReader
-        FileReader fileReader = new FileReader(filename);
-        // Create BufferedReader from FileReader
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        try {
-            // Read contents of file until end of file
-            while ((line = bufferedReader.readLine()) != null) {
-                // This is always true, but I'm leaving it to conform to the pattern.
-                if (firstNameIsValid(line)) {
-                    lineFirstName = line;
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-
-                if (lastNameIsValid(line)) {
-                    lineLastName = line;
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-                if (streetIsValid(line)) {
-                    lineStreet = line;
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-                if (cityIsValid(line)) {
-                    lineCity = line;
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-                if (stateIsValid(line)) {
-                    lineState = line;
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-                if (zipIsValid(line)) {
-                    // Must first convert string value in line to Integer.
-                    lineZip = Integer.valueOf(line);
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-                if (phoneIsValid(line)) {
-                    linePhone = line;
-                    // Input is valid so get the next line.
-                    line = bufferedReader.readLine();
-                }
-                else throw new IOException();
-                if (emailIsValid(line)) {
-                    lineEmail = line;
-                }
-                else throw new IOException();
-                // Call AddressEntry constructor for this group of related lines.
-                AddressEntry addressEntry = new AddressEntry(lineFirstName, lineLastName, lineStreet, lineCity, lineState, lineZip,
-                        linePhone, lineEmail);
-                // Add this entry to the address book.
-                ab.add(addressEntry);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // Close streams
-        bufferedReader.close();
-        fileReader.close();
-    }
-
-    /**
-     * Checks if the first name is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean firstNameIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if last name is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean lastNameIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if street address is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean streetIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if city is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean cityIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if state is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean stateIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if zip code is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean zipIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if phone number is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean phoneIsValid(String line) {
-        return (line != null);
-    }
-
-    /**
-     * Checks if email is valid.
-     * @param line Current line read from the file.  A <code>String</code>
-     * @return True if valid.  False if not.
-     */
-    private static boolean emailIsValid(String line) {
-        return (line != null);
     }
 }
---
+++ src/AddressBookTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void readFromFile() {
+    }
+}
